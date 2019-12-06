d1949dd6e66c4668937f3ef82ecb6b9883b420de
Manali Ghare <manalighare19@gmail.com>
2019-12-06 02:06:09 -0500

    Filter query parameter in API

--- routes/imageSearch.js
+++ routes/imageSearch.js
@@ -1,19 +1,32 @@
 const router = require('express').Router();
+const {searchValidation} = require('../validation');
 const  axios = require('axios');
 
 
 
-router.post('/search',async(req,res) => {
- 
+router.post('/image-search',async(req,res) => {
+    const { error } = searchValidation(req.body);
+    //Validation
+    if(error) return res.status(400).send({
+        status : res.statusCode,
+        message: error.details[0].message
+    });
+    let searchTerm = req.body.q;
     let config = {
         headers: { "Ocp-Apim-Subscription-Key": process.env.SubscriptionKey },
-        params: { q : 'tropical ocean' },
+        params: { q : searchTerm,
+                "imageType": req.body.imageType},
       }
+      console.log(searchTerm);
+      console.log(req.body.imageType);
+      
 
     await axios.get( process.env.endpoint , config ,
     
     ).then(response => {
         res.send(response.data);
+       
+        
     }).catch(err => {
         console.log(err);
 
---
+++ validation.js
@@ -0,0 +1,13 @@
+const Joi = require('@hapi/joi');
+
+//search term Validation
+const searchValidation = (data) =>{
+    const schema = Joi.object({
+        q: Joi.string().required(),
+        imageType: Joi.string().empty('').default('All')
+    });
+
+    return schema.validate(data);
+};
+
+module.exports.searchValidation = searchValidation;
